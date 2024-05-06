0ee10383622c6de7a17db0f635fbb052cea9b0ce
Tra My <namdatuoc1230@gmail.com>
2024-05-06 23:02:09 +0700

    commit Bai tap ve nha Buoi 3_20240506

---
+++ Buoi3/src/Bai4/Bai4_5.java
@@ -0,0 +1,16 @@
+package Bai4;
+
+import java.util.Scanner;
+
+public class Bai4_5 {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so thu nhat: ");
+        a = sc.nextInt();
+        System.out.print("Nhap so thu hai: ");
+        b = sc.nextInt();
+        int c = Math.min(a,b);
+        System.out.println("So nho nhat la: "+c);
+    }
+}
